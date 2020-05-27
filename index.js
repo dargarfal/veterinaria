6c7520b5972d0ab87e76b48243cbe8ea78fe063f
Ernesto Darge <ernesto@ernesto-lap.home>
2020-05-27 05:08:16 +0100

    agregado CORS

--- index.js
+++ index.js
@@ -3,8 +3,27 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 const routes = require('./routes');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
+
+//habilitar Cors para comunicacion entre servidores y restringiendo dominio
+app.use(cors()); //-> De esta manera es de acceso publico
+
+//De esta otra forma se restrinje solo el acceso a: http://locahost:30000
+const whitelist = ['http://localhost:3000']; //se pueden agregar otros dominios
+const corsOptions = {
+  origin: (origin, callback) => {
+    const existe =  whitelist.some( dominio => dominio === origin);
+    if(existe){
+      callback(null, true)
+    }else{
+      callback(new Error('No Permitido por CORS'))
+    }
+  }
+}
+
+//app.use(cors(corsOptions));
 
 //habilitar body-parser
 app.use(bodyParser.json());
